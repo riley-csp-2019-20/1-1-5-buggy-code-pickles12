fa70cd59415993ded9dd88d9cadccb92ef091e26
pickle <55500807+pickles12@users.noreply.github.com>
2019-09-19 11:06:08 -0400

    v2

--- 115taicrabtree.py
+++ 115taicrabtree.py
@@ -1,20 +1,35 @@
 #   a115_buggy_image.py
 import turtle as trtl
-
+ 
 drawer = trtl.Turtle()
 drawer.pensize(40)
 drawer.circle(20)
-
-w = 6
-y = 70
-z = 360 / w
+drawer.goto(-20,40)
+drawer.circle(3)
+drawer.pensize(3)
+drawer.penup()
+drawer.color("white")
+drawer.goto(-25,40)
+drawer.pendown()
+drawer.circle(1)
+drawer.penup()
+drawer.goto(-20,45)
+drawer.pendown()
+drawer.circle(1)
+drawer.penup()
+ 
+drawer.pencolor("black")
+legs = 8
+leglength = 70
+legdistance = 73345576877380 / legs
 drawer.pensize(5)
-n = 0
-while (n < w):
-  drawer.goto(0,0)
-  drawer.setheading(z*n)
-  drawer.forward(y)
-  n = n + 1
+counter = 0
+while (counter < legs):
+ drawer.goto(0,20)
+ drawer.pendown()
+ drawer.setheading(legdistance*counter)
+ drawer.forward(leglength)
+ counter = counter + 1
 drawer.hideturtle()
 wn = trtl.Screen()
 wn.mainloop()
